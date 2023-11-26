3e11ef8f414874edac3fb253dceb3eb4a037f96a
SAK74 <sergijks@gmail.com>
2023-11-26 11:08:02 +0100

    feat: create and fill tables with sdk

---
+++ backend/db/create.ts
@@ -0,0 +1,42 @@
+import { DynamoDBClient, CreateTableCommand } from "@aws-sdk/client-dynamodb";
+
+const client = new DynamoDBClient({ region: "eu-north-1" });
+
+const createProducts = new CreateTableCommand({
+  TableName: "Products",
+  KeySchema: [
+    { AttributeName: "id", KeyType: "HASH" },
+    { AttributeName: "title", KeyType: "RANGE" },
+  ],
+  AttributeDefinitions: [
+    { AttributeName: "id", AttributeType: "S" },
+    { AttributeName: "title", AttributeType: "S" },
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 1,
+    WriteCapacityUnits: 1,
+  },
+  Tags: [{ Key: "description", Value: "training-db" }],
+});
+
+const createStock = new CreateTableCommand({
+  TableName: "Stock",
+  KeySchema: [{ AttributeName: "product_id", KeyType: "HASH" }],
+  AttributeDefinitions: [{ AttributeName: "product_id", AttributeType: "S" }],
+  ProvisionedThroughput: { ReadCapacityUnits: 1, WriteCapacityUnits: 1 },
+});
+
+const main = async () => {
+  try {
+    const resp = await client.send(createProducts);
+    console.log("Products table: ");
+    console.log(JSON.stringify(resp, null, 2));
+  } catch {}
+  try {
+    const resp = await client.send(createStock);
+    console.log("Stock table: ");
+    console.log(JSON.stringify(resp, null, 2));
+  } catch {}
+};
+
+main();
---
+++ backend/db/data.ts
@@ -0,0 +1,38 @@
+type Product = {
+  title: string;
+  description: string;
+  price: number;
+};
+
+export const products: Product[] = [
+  {
+    description: "Short Product Description1",
+    price: 24,
+    title: "ProductOne",
+  },
+  {
+    description: "Short Product Description7",
+    price: 15,
+    title: "ProductTitle",
+  },
+  {
+    description: "Short Product Description2",
+    price: 23,
+    title: "Product",
+  },
+  {
+    description: "Short Product Description4",
+    price: 15,
+    title: "ProductTest",
+  },
+  {
+    description: "Short Product Descriptio1",
+    price: 23,
+    title: "Product2",
+  },
+  {
+    description: "Short Product Description7",
+    price: 15,
+    title: "ProductName",
+  },
+];
