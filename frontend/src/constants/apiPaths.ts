e7efca847ff30240a0aff09d42523e201f0685c9
SAK74 <sergijks@gmail.com>
2024-01-07 18:31:43 +0100

    feat: implement with frontend app

--- frontend/src/constants/apiPaths.ts
+++ frontend/src/constants/apiPaths.ts
@@ -1,9 +1,10 @@
 const API_PATHS = {
-  product: "https://oh69hmty09.execute-api.eu-north-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://puis95t8jd.execute-api.eu-north-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://uj2cycpqbe.execute-api.eu-north-1.amazonaws.com/dev",
+  order: "http://sak74-cart-api.eu-north-1.elasticbeanstalk.com/api",
+  import: "https://dy2vjez6d4.execute-api.eu-north-1.amazonaws.com/dev",
+  bff: "http://sak74-bff-api-dev.eu-north-1.elasticbeanstalk.com",
+  cart: "http://sak74-cart-api.eu-north-1.elasticbeanstalk.com/api",
+  // bff: "http://192.168.0.56:4001",
 };
 
 export default API_PATHS;
