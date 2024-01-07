af767c2b77f6e97d227f7d0c3349a81ccbaadea6
SAK74 <sergijks@gmail.com>
2024-01-07 18:08:04 +0100

    feat: deploy bff to beanstalk

--- backend/bff-service/src/main.ts
+++ backend/bff-service/src/main.ts
@@ -1,4 +1,4 @@
-import http from "http";
+import http, { RequestOptions } from "http";
 import https from "https";
 
 if (process.env.NODE_ENV === "development") {
@@ -8,94 +8,82 @@ if (process.env.NODE_ENV === "development") {
 const ERROR_502 = "Cannot process request";
 
 const PORT = process.env.PORT || 4001;
-// console.log("test: ", process.env);
 
 const server = http.createServer((req, resp) => {
   resp.setHeader("access-control-allow-origin", "*");
+  resp.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent"
+  );
+  resp.setHeader("Access-Control-Allow-Credentials", "true");
+  resp.setHeader("Access-Control-Allow-Methods", "OPTIONS,GET,PUT");
 
-  console.log("Req url: ", req.url);
+  try {
+    const recipient = req.url?.split("/")[1].split("?")[0];
+    const recipientUrl = process.env[recipient || ""];
+    // console.log("recipient url: ", recipientUrl);
 
-  const recipient = req.url?.split("/")[1].split("?")[0];
-  console.log("recipient: ", recipient);
-  const recipientUrl = process.env[recipient || ""];
-  console.log("recipient url: ", recipientUrl);
+    if (!recipientUrl) {
+      resp.statusCode = 502;
+      resp.statusMessage = ERROR_502;
+      resp.end(JSON.stringify({ message: ERROR_502 }));
+    } else {
+      const outgoingReqPath = req.url?.replace(`/${recipient}`, "");
 
-  if (!recipientUrl) {
-    //to change
-    resp.statusCode = 502;
-    resp.statusMessage = ERROR_502;
-    resp.end(JSON.stringify({ message: ERROR_502 }));
-  } else {
-    // const useHttps = false;
+      const httpModule = recipient === "cart" ? http : https;
 
-    const outgoingReqPath = req.url?.replace(`/${recipient}`, "");
-    console.log("out url: ", outgoingReqPath);
-
-    const httpModule = recipient === "cart" ? http : https;
-
-    let body = "";
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
-    req.on("end", () => {
-      // console.log("End of request! Body: ", body, typeof body);
-      console.log("Path summary: ", recipientUrl + outgoingReqPath);
-      const outgoingReq = httpModule.request(
-        // "https://randomuser.me/api/",
-        // "http://localhost:3002",
-        // "https://uj2cycpqbe.execute-api.eu-north-1.amazonaws.com/dev" +
-        //   "/products",
-        recipientUrl + outgoingReqPath,
-        {
-          method: req.method,
-          // path: req.url,
-          headers: {
-            // "Content-ty"
-            // connection: "keep-alive",
-            // "Content-Type": ["application/json"],
-            authorization: req.headers.authorization,
-          },
-          // auth: req.headers.authorization,
-        },
-        (outResp) => {
-          const status = outResp.statusCode || 500;
-          console.log("Status: ", status);
-          resp.statusCode = status;
-          outResp.on("data", (data) => {
-            console.log("Data: ", data.toString());
-            resp.write(data);
-          });
-          outResp.on("error", (err) => {
-            console.log("Error: ", err.message);
-            // resp.statusCode = status;
-            resp.write(err.message || http.STATUS_CODES[status]);
-            resp.end();
-          });
-          outResp.on("end", () => {
-            // resp.statusCode = status;
-            resp.end();
-          });
-        }
-      );
-      // console.log(outgoingReq);
-      if (body) {
-        console.log("Body: ", body);
-        outgoingReq.write(body);
-        // console.log("Headers: ", req.headers);
-      }
-      // console.log(req.url);
-      console.log("outgoing URL: ", outgoingReq.path);
-      outgoingReq.on("error", (error) => {
-        console.log("Error: ", error);
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk;
       });
-      outgoingReq.end();
-    });
-    // resp.writeHead(200, {
-    //   "access-control-allow-origin": "*",
-    // });
+      req.on("end", () => {
+        const options: RequestOptions = {
+          method: req.method,
+          headers: {},
+        };
+        if (req.headers["authorization"]) {
+          options.headers!.authorization = req.headers.authorization;
+        }
+        // console.log("Path summary: ", recipientUrl + outgoingReqPath);
+        const outgoingReq = httpModule.request(
+          recipientUrl + outgoingReqPath,
+          options,
+          (outResp) => {
+            const status = outResp.statusCode || 500;
+            // console.log("Status: ", status, outResp.statusMessage);
+            resp.statusCode = status;
+            outResp.on("data", (data) => {
+              // console.log("Data: ", data.toString());
+              resp.write(data);
+            });
+            outResp.on("error", (err) => {
+              console.log("Error: ", err.message);
+              resp.write(err.message || http.STATUS_CODES[status]);
+              resp.end();
+            });
+            outResp.on("end", () => {
+              // resp.statusCode = status;
+              resp.end();
+            });
+          }
+        );
+        if (body) {
+          outgoingReq.write(body);
+        }
 
-    // resp.statusCode = 200;
-    // resp.end("Hello!");
+        outgoingReq.on("error", (error) => {
+          console.log("Error: ", error.message);
+          resp.statusCode = 400; //
+          resp.statusMessage = http.STATUS_CODES[400] || "";
+          resp.end(error.message);
+        });
+        outgoingReq.end();
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    resp.statusCode = 500;
+    resp.end((err as Error).message);
   }
 });
 
