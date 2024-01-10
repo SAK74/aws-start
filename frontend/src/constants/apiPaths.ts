d8b33dac603ceab4845a2baaa280e5376785176d
SAK74 <sergijks@gmail.com>
2024-01-10 20:18:51 +0100

    perf: deploy FE with proxy API

--- frontend/src/constants/apiPaths.ts
+++ frontend/src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://uj2cycpqbe.execute-api.eu-north-1.amazonaws.com/dev",
   order: "http://sak74-cart-api.eu-north-1.elasticbeanstalk.com/api",
   import: "https://dy2vjez6d4.execute-api.eu-north-1.amazonaws.com/dev",
-  bff: "http://sak74-bff-api-dev.eu-north-1.elasticbeanstalk.com",
+  bff: "https://iw3pzc9uec.execute-api.eu-north-1.amazonaws.com/",
   cart: "http://sak74-cart-api.eu-north-1.elasticbeanstalk.com/api",
   // bff: "http://192.168.0.56:4001",
 };
